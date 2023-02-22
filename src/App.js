832f5140e297d162e2da0141942b3c0293f6949a
Habtamu210 <beleyaha@gmail.com>
2023-02-22 10:28:20 -0800

    Add feedbacks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,37 +1,26 @@
-import React from 'react';
-
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="keys screen"> 0 </div>
-        <button type="button" className="keys">AC</button>
-        <button type="button" className="keys">+/-</button>
-        <button type="button" className="keys">%</button>
-        <button type="button" className="keys operators">/</button>
-        <button type="button" className="keys ">7</button>
-        <button type="button" className="keys">8</button>
-        <button type="button" className="keys">9</button>
-        <button type="button" className="keys operators">x</button>
-        <button type="button" className="keys">4</button>
-        <button type="button" className="keys">5</button>
-        <button type="button" className="keys">6</button>
-        <button type="button" className="keys operators">-</button>
-        <button type="button" className="keys">1</button>
-        <button type="button" className="keys">2</button>
-        <button type="button" className="keys">3</button>
-        <button type="button" className="keys operators">+</button>
-        <button type="button" className="keys zero">0</button>
-        <button type="button" className="keys">.</button>
-        <button type="button" className="keys operators">=</button>
-      </div>
-    );
-  }
-}
+const Calculator = () => (
+  <div className="mains">
+    <div className="output">0</div>
+    <div className="keys">AC</div>
+    <div className="keys">+/-</div>
+    <div className="keys">%</div>
+    <div className="sign">÷</div>
+    <div className="keys">7</div>
+    <div className="keys">8</div>
+    <div className="keys">9</div>
+    <div className="sign">*</div>
+    <div className="keys">4</div>
+    <div className="keys">5</div>
+    <div className="keys">6</div>
+    <div className="sign">-</div>
+    <div className="keys">1</div>
+    <div className="keys">2</div>
+    <div className="keys">3</div>
+    <div className="sign">+</div>
+    <div className="keys null">0</div>
+    <div className="keys">.</div>
+    <div className="sign">=</div>
+  </div>
+);
 
 export default Calculator;
