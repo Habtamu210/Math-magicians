83a0d2a06b2919afd0101f4cc167a0ef050d3e38
Habtamu210 <beleyaha@gmail.com>
2023-02-22 22:05:22 -0800

    Add event functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="background" />
-      <Calculator />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calculatorData: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+    this.updateState = this.updateState.bind(this);
+  }
 
+  updateState(obj) {
+    this.setState({
+      calculatorData: obj,
+    });
+  }
+
+  render() {
+    const { calculatorData } = this.state;
+    return (
+      <div className="App">
+        <Calculator calculatorData={calculatorData} updateState={this.updateState} />
+      </div>
+    );
+  }
+}
 export default App;
