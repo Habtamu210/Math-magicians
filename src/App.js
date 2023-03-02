877f9cc1976a1764cd6c864a39c24711fa901f4a
Habtamu210 <beleyaha@gmail.com>
2023-03-01 23:32:56 -0800

    Full website for calculator app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navigation from './components/navagation';
+import Home from './components/home';
+import Quote from './components/quote';
+import Default from './components/default';
 
 function App() {
   return (
-    <div className="App">
-      <div className="background" />
-      <Calculator />
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route element={<div className="back-page" />} />
+        <Route path="/mathmatica" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="*" element={<Default />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
